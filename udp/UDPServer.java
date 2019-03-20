5cbaaec0a7ab489e378ade223a7f496ba730aaa8
Gabriele Civitarese <gabriele.civitarese@unimi.it>
2019-03-20 10:51:55 +0100

    pulizia strutture dati

--- udp/UDPServer.java
+++ udp/UDPServer.java
@@ -7,11 +7,14 @@ class UDPServer {
 
 		DatagramSocket serverSocket = new DatagramSocket(9876); 
 
-		byte[] receiveData = new byte[1024]; 
-		byte[] sendData  = new byte[1024]; 
+		byte[] receiveData; 
+		byte[] sendData;
 
 		while(true) 
 		{ 
+
+			receiveData = new byte[1024];
+			sendData = new byte[1024];
 
 			/* Prepara la struttura dati usata per contenere il pacchetto in ricezione */
 			DatagramPacket receivePacket = 
